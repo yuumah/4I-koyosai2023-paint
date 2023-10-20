1b0d0f1a724c84e5e323922c58a07a193ada15d2
uscuber <sota1210nittc@gmail.com>
2023-10-20 18:14:38 +0900

    画像投稿用のページの枠組みを作成

--- server/server.js
+++ server/server.js
@@ -6,8 +6,32 @@ const PORT = process.env.PORT || 7000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const token = require("./token.json").token;
+
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("OK");
+});
+
+app.get("/uploadImages", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  if(req.query.token !== token){
+    res.end("query parameter is invalid");
+    return;
+  }
+  res.end("get uploadImages");
+});
+
+app.post("/uploadImages", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  if(req.query.token !== token){
+    res.end("query parameter is invalid");
+    return;
+  }
+
+  console.log(req.body);
+
   res.end("OK");
 });
 
