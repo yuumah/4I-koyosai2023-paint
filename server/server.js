776c2ce1dc4e567a49ec0479f4f4dfb605cb88f6
uscuber <sota1210nittc@gmail.com>
2023-10-25 10:03:29 +0900

    画像取得のプログラムの追加

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
+const glob = require("glob");
 
 const PORT = process.env.PORT || 7000;
 const app = express();
@@ -8,11 +9,39 @@ app.use(bodyParser.urlencoded({ limit: "8mb", extended: true }));
 app.use(bodyParser.json());
 
 const token = require("./token.json").token;
+const image_types = ["flower", "butterfly"];
 
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("OK");
 });
+
+
+// 画像の取得
+app.get("/images", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  if(req.query.token !== token){
+    res.end("query parameter is invalid");
+    return;
+  }
+  const type = req.query.type;
+  let is_valid = false;
+  image_types.forEach((t) => {
+    if(t === type) is_valid = true;
+  });
+  if(!is_valid){
+    res.end("type is invalid");
+    return;
+  }
+  const images = glob.sync(`images/${type}/*.png`);
+  let encoded_images = [];
+  images.forEach((image) => {
+    const data = fs.readFileSync(image);
+    encoded_images.push(data.toString("base64"));
+  });
+  res.end(JSON.stringify(encoded_images));
+});
+
 
 app.get("/uploadImages", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
