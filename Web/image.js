847868c60787e96fff5b8c34e30ee2847f7f183c
nakakonakako <kotamina2016@gmail.com>
2023-10-29 17:09:15 +0900

    flower, balloon以外にも動き追加 && まれにあるballoonの不具合修正

--- Web/image.js
+++ Web/image.js
@@ -1,7 +1,7 @@
 let index = 0;
 let imgindex = 0;
 let repeatCount = 30;
-let previousImage = null;
+var previousImage = null;
 let balloons = [];
 let allimages = ["flower", "balloon", "turtle", "butterfly", "elephant"];
 let randomX = 0;
@@ -14,23 +14,27 @@ function loadImage(index) {
 		img.src = "http://tk2-233-26371.vs.sakura.ne.jp:7000/images?type="+ allimages[imgindex] +"&index=" + index;
 		img.onload = () => {
       if(allimages[imgindex] !== "balloon"){
-        randomY = Math.floor(Math.random() * 50) + 35;
+        randomY = (allimages[imgindex] !== "elephant") ? Math.floor(Math.random() * 53) + 35 : Math.floor(Math.random() * 40 + 30);
       }
       else{
         img.id = "balloon" + index + "";
         randomY = Math.floor(Math.random() * 20);
         balloons.push(img);
       }
-      img.style.left = Math.floor(Math.random() * 91) + "%";
+      randomX = Math.floor(Math.random() * 90);
+      img.style.left = randomX + "%";
       img.style.top = randomY + "%";
-      img.width = "100";
+      img.width = (allimages[imgindex] !== "elephant") ? "100" : "250";
       previousImage = img;
       document.body.appendChild(img);
 			resolve(index); // 画像の読み込みが成功した場合
 		};
 	
 		img.onerror = () => {
-      previousImage.width = "150";
+      previousImage.width = (allimages[imgindex] !== "elephant") ? "150" : "275";
+      previousImage.style.left = Math.floor(Math.random() * 40) + 20 + "%"; 
+      previousImage.style.top = (allimages[imgindex] !== "balloon") ? "50%" : "10%";
+      document.body.appendChild(previousImage);
 			reject(index); // 画像の読み込みが失敗した場合
 		};
 	});
@@ -52,24 +56,23 @@ async function loadImages() {
     }
     index++;
   }
-
-  moveImage();
 }
 
 
 function moveImage() {
-  let leftArray = balloons.map(item => (item.offsetLeft / window.innerWidth) * 100);
-  let modifiedArray = leftArray.map(item => item + 0.3);
-  const indexArrayGreaterThan93 = modifiedArray.map((item, index) => item >= 91 ? index : -1).filter(index => index !== -1);
+  let leftArray = balloons.map(item => ((item.offsetLeft / window.innerWidth) * 100));
+  let modifiedArray = leftArray.map(item => item + 0.05);
+  const indexArrayGreater = modifiedArray.map((item, index) => item >= 91 ? index : -1).filter(index => index !== -1);
   balloons.forEach((balloon, index) => {
     balloon.style.left = modifiedArray[index] + "%";
   });
-  if(indexArrayGreaterThan93.length > 0){
-    for(const index of indexArrayGreaterThan93){
+  if(indexArrayGreater.length > 0){
+    for(let index of indexArrayGreater){
       balloons[index].style.left = "0%";
     }
   }
-  setTimeout(moveImage, 50);
+  requestAnimationFrame(moveImage)
 }
 
-setTimeout(loadImages, 100);
+loadImages();
+requestAnimationFrame(moveImage);
