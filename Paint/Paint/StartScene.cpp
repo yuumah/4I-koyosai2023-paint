5e8c5a8c17b275413625510cb00295ff6cf1bf78
tamagosushio <mitachi.ts75@gmail.com>
2023-10-31 00:23:04 +0900

    [change] 開始ボタンを変更

--- Paint/Paint/StartScene.cpp
+++ Paint/Paint/StartScene.cpp
@@ -6,10 +6,38 @@ StartScene::StartScene(const InitData &init) : IScene(init){
 }
 
 void StartScene::draw(void) const {
+	Rect rect = draw_button_rect(button_rect, button_hsv, button_shadow_hsv);
+	draw_button_label(U"開始", rect, Palette::White);
 }
 
 void StartScene::update(void){
-	if(SimpleGUI::Button(U"始める", Scene::Center())){
+	if(button_rect.leftReleased()){
 		changeScene(U"ReelScene", 1.0s);
 	}
+}
+
+Rect StartScene::draw_button_rect(Rect rect, HSV button_color, const HSV& shadow_color)const {
+    Vec2 shadow{ 0, rect.h / 7.5 };
+    if (rect.mouseOver()) {
+        button_color = button_color.setS(button_color.s * 4 / 5);
+    }
+    if (rect.leftPressed()) {
+        rect = rect.stretched(-shadow.y, 0, shadow.y, 0);
+        rect.rounded(100).draw(button_color);
+    }else {
+        rect.rounded(100).drawShadow(shadow, 5, 0, shadow_color).draw(button_color);
+    }
+    return rect;
+}
+void StartScene::draw_button_label(const String& label, const Rect& rect, const HSV& color)const {
+    int left = 0, right = 1000;
+    while (Abs(right - left) > 1) {
+        int mid = (left + right) / 2;
+        if (font(label).draw(mid, rect.stretched(-rect.h / 20), HSV{ 0,0 })) {
+            left = mid;
+        }else {
+            right = mid;
+        }
+    }
+	font(label).drawAt(left, rect.center(), color);
 }
--- Paint/Paint/StartScene.hpp
+++ Paint/Paint/StartScene.hpp
@@ -7,4 +7,12 @@ public:
 	StartScene(const InitData &init);
 	void draw(void) const;
 	void update(void);
+private:
+	Font font{ 200, U"SourceHanSansJP-Medium.otf" };
+	// 開始ボタンの描画
+	Rect draw_button_rect(Rect rect, HSV button_color, const HSV &shadow_color)const;
+	void draw_button_label(const String &label, const Rect &rect, const HSV &color)const;
+	Rect button_rect{ Arg::bottomCenter(Scene::Center().x, Scene::Size().y * 9 / 10), Scene::Size() / 3 };
+	HSV button_hsv{ 200, 0.75, 0.85 };
+	HSV button_shadow_hsv{ 200, 0.75, 0.65 };
 };
