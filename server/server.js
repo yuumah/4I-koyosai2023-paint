dc2b6a10ff5ef799a127635a6528cab0f47a51fc
sota <sota1210nittc@gmail.com>
2023-11-05 01:11:08 +0900

    windowsのpathの形式に対応(バグ修正)

--- server/server.js
+++ server/server.js
@@ -11,8 +11,10 @@ app.use(bodyParser.json());
 
 const token = require("./token.json").token;
 const image_types = glob.sync("images/*").map((path) => {
-  return path.substring(path.indexOf("/")+1);
+  if(path.indexOf("/") !== -1) return path.substring(path.indexOf("/")+1);
+  return path.substring(path.indexOf("\\")+1);
 });
+console.log(image_types);
 
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
